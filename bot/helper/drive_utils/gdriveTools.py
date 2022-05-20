70000faa6e93554c41de00a8b584bd31b4c15094
majnurangeela <84721324+majnurangeela@users.noreply.github.com>
2022-05-20 19:13:10 +0530

    Update gdriveTools.py

--- bot/helper/drive_utils/gdriveTools.py
+++ bot/helper/drive_utils/gdriveTools.py
@@ -23,7 +23,7 @@ from tenacity import *
 from bot import LOGGER, DRIVE_NAME, DRIVE_ID, INDEX_URL, parent_id, \
     IS_TEAM_DRIVE, telegraph, USE_SERVICE_ACCOUNTS, DRIVE_INDEX_URL
 from bot.helper.ext_utils.bot_utils import *
-from bot.helper.telegram_helper import button_builder
+from bot.helper.telegram_helper.button_build import ButtonMaker
 
 logging.getLogger('googleapiclient.discovery').setLevel(logging.ERROR)
 
@@ -235,11 +235,11 @@ class GoogleDriveHelper:
                 msg += f"\n<b>SubFolders: </b>{self.total_folders}"
                 msg += f"\n<b>Files: </b>{self.total_files}"
                 durl = self.__G_DRIVE_DIR_BASE_DOWNLOAD_URL.format(dir_id)
-                button = ButtonMaker()
-                buttons.build_button("☁️ Drive Link", durl)
+                buttons = ButtonMaker()
+                buttons.buildbutton("☁️ Drive Link", durl)
                 if DRIVE_INDEX_URL is not None:
                     url = requests.utils.requote_uri(f'{DRIVE_INDEX_URL}/{meta.get("name")}/')
-                    buttons.build_button("⚡ Index Link", url)
+                    buttons.buildbutton("⚡ Index Link", url)
             else:
                 file = self.copyFile(meta.get('id'), parent_id, status)
                 try:
@@ -535,6 +535,6 @@ class GoogleDriveHelper:
               f"<b>(Time taken {time_taken}s)</b>"
 
         buttons = button_builder.ButtonMaker()
-        buttons.build_button("VIEW RESULTS 🗂️", f"https://telegra.ph/{self.path[0]}")
+        buttons.buildbutton("VIEW RESULTS 🗂️", f"https://telegra.ph/{self.path[0]}")
 
         return msg, InlineKeyboardMarkup(buttons.build_menu(1))
