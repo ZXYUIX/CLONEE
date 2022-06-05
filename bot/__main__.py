8939e879675afb46724ebaf2d065eb45eef281bb
David Thomas <37041171+ZXYUIX@users.noreply.github.com>
2022-06-06 01:54:16 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -47,7 +47,7 @@ For <i>file</i> results only:
 
 /{BotCommands.BypassCommand} [shorten_url]: Bypass GPLinks, RockLinks, Adf.ly and DropLink Links
 
-/{BotCommands.PermissionCommand} [drive_url]: Set data permission to 'Anyone with the link' (Only owner)
+/{BotCommands.PermissionCommand} [drive_url] [email]: Set data permission of Drive (Email optional & Only owner)
 
 /{BotCommands.DeleteCommand} [drive_url]: Delete data from Drive (Only owner)
 
